dd8b5aa4faf15a8896a68e01b4336a30447e3804
and-raf <kontakt@andraf.pl>
2016-10-14 17:19:48 +0200

    .

--- php/db.php
+++ php/db.php
@@ -8,15 +8,25 @@
 class Db{
     function connect(){
         try {
-            $conn = new PDO("mysql:host='localhost';dbname='cms'", 'root', '');
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//            $conn = new PDO('mysql:host=localhost;dbname=cms', 'root', '');
+//            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             echo 'Connected';
         } catch
         (PDOException $e) {
             echo "Error: " . $e->getMessage();
         };
-
+    }
+    function register($user, $password){
+        $conn = new PDO('mysql:host=localhost;dbname=cms', 'root', '');
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $register = $conn->prepare("INSERT INTO users (uname, pass) 
+    VALUES (:uname, :pass)");
+        $register->bindParam(':uname', $user);
+        $register->bindParam(':pass', $password);
+        $register->execute();
+    }
+    function quit(){
         $conn = null;
     }
 }
--- php/register.php
+++ php/register.php
@@ -4,4 +4,14 @@
 
     echo $twig->render('container.twig');
     echo $twig->render('register.twig');
+
+    $uname = isset($_POST['login']);
+    $pass = isset($_POST['pwd']);
+
+    $help = $_POST['login'];
+    if ($help){
+//        Db::connect();
+        Db::register($uname, $pass);
+        Db::quit();
+    }
 ?>
